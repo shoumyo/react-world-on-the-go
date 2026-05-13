e13ce8ffdb9d74766c717c5a86e62b4eb7ae5ead
Shoumyo <shoumyosrijito@gmail.com>
2026-05-13 18:14:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from './Components/Countries/countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h4>React on the Go</h4>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
